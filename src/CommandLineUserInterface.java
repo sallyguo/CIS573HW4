8456a8eff45b871e90cf36e89bf5300fea92b633
Andy Ye <andyye@seas.upenn.edu>
2011-12-10 17:04:33 -0500

    cli refactor

--- src/CommandLineUserInterface.java
+++ src/CommandLineUserInterface.java
@@ -1,26 +1,155 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 
-public class CommandLineUserInterface extends UserInterface {
+public class CommandLineUserInterface implements UserInterface {
 	
 	protected Scanner _in;
+	protected PrintStream _out;
+	protected Log _log;
+	protected DataProcessor _dataProcessor;
 	
-	public CommandLineUserInterface() {
-		super();
-		_out = System.out;
+	public CommandLineUserInterface(DataProcessor dataProcessor) {
+		_log = Log.getInstance();
 		_in = new Scanner(System.in);
+		_out = getPrintstream();
+		_dataProcessor = dataProcessor;
 	}
 	
-	public static void main(String[] args) {
-		UserInterface ui = new CommandLineUserInterface();
-		ui.start();
+	public void start() {
+		
+		println("Welcome to the World Series database!");
+		_log.log("Starting application");
+
+		String choice = null; // the thing that the user chooses to do
+
+		do {
+			showPrompt();
+			
+			choice = getInputString();
+			_log.log("User input: " + choice);
+			
+			if (choice.equals("1")) searchByYear();	
+			else if (choice.equals("2")) searchByTeam();
+			else if (choice.equals("3")) searchByRange();
+			else if (choice.equals("4")) showTeamsYears();
+			else if (!quit(choice)) {
+				println("That is not a valid selection.");
+			}
+			
+		}
+		while (!quit(choice));
+		println("Good bye");
+	}
+
+	protected static boolean quit(String choice) {
+		return choice.equals("Q") || choice.equals("q");
 	}
 	
-	protected String getInputString() {
+	public void showPrompt() {
+		println("");
+		println("Please make your selection:");
+		println("1: Search for World Series info by year");
+		println("2: Search for World Series info by team");
+		println("3: Show all World Series for a range of years");
+		println("4: Show all teams' World Series wins");
+		println("Q: Quit");
+		print("> ");
+
+	}
+
+	public void searchByYear() {
+		print("Please enter the year: ");
+		try {
+			int year = getInputInt();
+			_log.log("Trying to search for year: " + year);
+			WorldSeriesInstance wsi = _dataProcessor.showDataForYear(year);
+			if (wsi == null) println("No World Series held in " + year);
+			else println(wsi.toString());
+		}
+		catch (Exception e) { 
+			println("That is not a valid year.");
+			_log.log("User entered invalid year");
+		}
+	}
+
+	public String askUserForWinLossOrAll() {
+		print("Do you want to see World Series the team has (W)on, (L)ost, or (A)ll? ");
+		return getInputString();
+	}
+	
+	public String askUserForTeamName() {
+		print("Please enter the team name: ");
+		return getInputString();
+	}
+
+	public void searchByTeam() {
+		String team = askUserForTeamName();
+		String which = askUserForWinLossOrAll();
+		_log.log("Trying to search for: team=" + team + ", which=" + which);
+
+		if (which.equalsIgnoreCase("W"))
+			println(_dataProcessor.showDataForTeamWins(team));
+		else if (which.equalsIgnoreCase("L"))
+			println(_dataProcessor.showDataForTeamLosses(team));
+		else if (which.equalsIgnoreCase("A"))
+			println(_dataProcessor.showDataForTeamAll(team));
+		else
+			println(which + " is not a valid selection.");
+	}
+	
+	public void searchByRange() {
+		int startYear, endYear;
+		print("Please enter the starting year: ");
+		try {
+			startYear = getInputInt();
+			_log.log("Trying to search for range starting with: " + startYear);
+		}
+		catch (Exception e) { 
+			println("That is not a valid year.");
+			_log.log("User entered invalid year");
+			return;
+		}
+		print("Please enter the ending year: ");
+		try {
+			endYear = getInputInt();
+			_log.log("Trying to search for range starting with: " + endYear);
+		}
+		catch (Exception e) { 
+			println("That is not a valid year.");
+			_log.log("User entered invalid year");
+			return;
+		}
+		String yearData = _dataProcessor.showDataForRange(startYear, endYear);
+		println(yearData);
+	}
+	
+	public void showTeamsYears(){
+		String teamYearsData = _dataProcessor.showDataTeamsYears();
+		println(teamYearsData);
+	}
+	
+	public void print(String data) {
+		_out.print(data);
+	}
+
+	public void println(int data) {
+		_out.println(data);
+	}
+
+	public void println(String data) {
+		_out.println(data);
+	}
+	
+	public String getInputString() {
 		return _in.nextLine();
 	}
 
-	protected int getInputInt() {
+	public int getInputInt() {
 		return _in.nextInt();
+	}
+	
+	public PrintStream getPrintstream() {
+		return System.out;
 	}
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,149 +8,33 @@ import java.io.PrintStream;
  * It is up to a subclass to initialize the PrintStream to the appropriate instance.
  */
 
-public abstract class UserInterface {
-	
-	protected PrintStream _out; // subclasses must instantiate this
-	
-	// subclasses must implement these methods
-	protected abstract String getInputString();
-	protected abstract int getInputInt();
+public interface UserInterface {
 
-	private DataStore _dataStore;
-	private DataProcessor _dataProcessor;
+	public String getInputString();
 	
-	// map of teams to the years they won
-	private Log log;
-	
-	protected UserInterface() {
-		_dataStore = new DataStore("WorldSeries.csv");
-		_dataProcessor = new DataProcessor(_dataStore);
-		log = new Log();
-	}
+	public int getInputInt();
 			
-	public void start() {
-		
-		println("Welcome to the World Series database!");
-		log.log("Starting application");
-
-		String choice = null; // the thing that the user chooses to do
-
-		do {
-			showPrompt();
-			
-			choice = getInputString();
-			log.log("User input: " + choice);
-			
-			if (choice.equals("1")) searchByYear();	
-			else if (choice.equals("2")) searchByTeam();
-			else if (choice.equals("3")) searchByRange();
-			else if (choice.equals("4")) showTeamsYears();
-			else if (!quit(choice)) {
-				println("That is not a valid selection.");
-			}
-			
-		}
-		while (!quit(choice));
-		println("Good bye");
-	}
-
-	protected static boolean quit(String choice) {
-		return choice.equals("Q") || choice.equals("q");
-	}
+	public void start();
 	
-	protected void showPrompt() {
-		println("");
-		println("Please make your selection:");
-		println("1: Search for World Series info by year");
-		println("2: Search for World Series info by team");
-		println("3: Show all World Series for a range of years");
-		println("4: Show all teams' World Series wins");
-		println("Q: Quit");
-		print("> ");
+	public void showPrompt();
 
-	}
+	public void searchByYear();
 
-	protected void searchByYear() {
-		print("Please enter the year: ");
-		try {
-			int year = getInputInt();
-			log.log("Trying to search for year: " + year);
-			WorldSeriesInstance wsi = _dataProcessor.showDataForYear(year);
-			if (wsi == null) println("No World Series held in " + year);
-			else println(wsi.toString());
-		}
-		catch (Exception e) { 
-			println("That is not a valid year.");
-			log.log("User entered invalid year");
-		}
-	}
-
-	protected String askUserForWinLossOrAll() {
-		print("Do you want to see World Series the team has (W)on, (L)ost, or (A)ll? ");
-		return getInputString();
-
-	}
+	public String askUserForWinLossOrAll();
 	
-	protected String askUserForTeamName() {
-		print("Please enter the team name: ");
-		return getInputString();
-	}
+	public String askUserForTeamName();
 
-	protected void searchByTeam() {
-		String team = askUserForTeamName();
-		String which = askUserForWinLossOrAll();
-		log.log("Trying to search for: team=" + team + ", which=" + which);
-
-		if (which.equalsIgnoreCase("W"))
-			println(_dataProcessor.showDataForTeamWins(team));
-		else if (which.equalsIgnoreCase("L"))
-			println(_dataProcessor.showDataForTeamLosses(team));
-		else if (which.equalsIgnoreCase("A"))
-			println(_dataProcessor.showDataForTeamAll(team));
-		else
-			println(which + " is not a valid selection.");
-	}
+	public void searchByTeam();
 	
-	protected void searchByRange() {
-		int startYear, endYear;
-		print("Please enter the starting year: ");
-		try {
-			startYear = getInputInt();
-			log.log("Trying to search for range starting with: " + startYear);
-		}
-		catch (Exception e) { 
-			println("That is not a valid year.");
-			log.log("User entered invalid year");
-			return;
-		}
-		print("Please enter the ending year: ");
-		try {
-			endYear = getInputInt();
-			log.log("Trying to search for range starting with: " + endYear);
-		}
-		catch (Exception e) { 
-			println("That is not a valid year.");
-			log.log("User entered invalid year");
-			return;
-		}
-		String yearData = _dataProcessor.showDataForRange(startYear, endYear);
-		println(yearData);
-	}
+	public void searchByRange();
 	
-	protected void showTeamsYears(){
-		String teamYearsData = _dataProcessor.showDataTeamsYears();
-		println(teamYearsData);
-	}
+	public void showTeamsYears();
 	
-	public void print(String data) {
-		_out.print(data);
-	}
+	public void print(String data);
 
-	public void println(int data) {
-		_out.println(data);
-	}
+	public void println(int data);
 
-	public void println(String data) {
-		_out.println(data);
-	}
+	public void println(String data);
+	
+	public PrintStream getPrintstream();
 }
